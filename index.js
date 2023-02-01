f3dc2f565b6dad5c1d7bb89ff6011283991b69e2
Brian Pelaez <pelaez_brian@hotmail.com>
2023-02-01 09:19:37 -0300

    API WHOAMI

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use('/', (req, res, next) => {
+  console.log(req.method + " "+req.path)
+  next();
+})
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
 });
 
 // listen for requests :)
